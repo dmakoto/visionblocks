76955e7a002b3c4ef2c75c67e0b951df11381079
nickmm <nickmm@mit.edu>
2013-12-19 18:43:20 -0500

    Adding TouchListener

---
+++ vblocks-base/src/edu/mit/cameraCulture/vblocks/ModuleTouchListener.java
@@ -0,0 +1,9 @@
+package edu.mit.cameraCulture.vblocks;
+
+import android.view.GestureDetector;
+import android.view.View;
+
+public interface ModuleTouchListener extends View.OnTouchListener,
+		View.OnClickListener, GestureDetector.OnDoubleTapListener {
+
+}
